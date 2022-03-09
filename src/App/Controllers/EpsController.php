4fd84c7115ef249db3f93faa112cf92e4debac84
HeilerNova <81369584+HeilerNova@users.noreply.github.com>
2022-03-09 15:51:26 -0500

    Actialización de los comandos del controlador

--- src/App/Controllers/EpsController.php
+++ src/App/Controllers/EpsController.php
@@ -7,12 +7,15 @@
 namespace App\Controllers;
 
 use App\AppBaseController;
+use App\Models\EpsModel;
+use App\Templates\Db\EpsDB;
 use HNova\Api\Http\ResponseApi;
 use HNova\Api\Response;
 
 class EpsController extends AppBaseController
 {
-    public function __construct(private EpsModel $EpsModel)
+    private EpsModel $EpsModel;
+    public function __construct()
     {
         $this->EpsModel = new EpsModel();
         parent::__construct();
@@ -35,6 +38,35 @@ class EpsController extends AppBaseController
             $res->message->content[] = "No se encontro ninguna infromación de la EPS";
             return new Response($res);
         }
+    }
+
+    function insert():Response
+    {
+        $name_eps = (string)$this->getBody();
+        
+        $ok = $this->EpsModel->validName($name_eps);
+        $res = new ResponseApi();
+
+        if (!$ok){
+            $eps = new EpsDB();
+            $eps->name = $name_eps;
+            // return new Response($eps);
+            $eps = $this->EpsModel->add($eps);
+
+            if ($eps){
+
+                $res->data = $eps;
+            }else{
+
+                $res->message->content[] = "No se pudo registrar";
+                $res->message->content[] = "Por favir intentelo nuevamente";
+            }
+        }else{
+            // En coso de que este registrado
+            $res->message->content[] = "La EPS ya se encuentra registrada.";
+        }
+
+        return new Response($res);
     }
 
     function desable(int $id):Response
