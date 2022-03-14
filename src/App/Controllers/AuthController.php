0f96dc32382841a35350771bc6332304f6606185
HeilerNova <81369584+HeilerNova@users.noreply.github.com>
2022-03-14 15:38:02 -0500

    Se agrego la validación que usuarios bloquedaso en la autenticación

--- src/App/Controllers/AuthController.php
+++ src/App/Controllers/AuthController.php
@@ -27,26 +27,34 @@ class AuthController extends AppBaseController
         $res->message->type = 1;
 
         if ($user){
-            if (password_verify($data->password, $user['password'])){
-                
-                // En caso de que la constraseña sea correcta.
-                $toke = ApiFunctions::generateToken(50);
-                $ok = $this->database->update(['token'=>$toke], ["id=?", $user['id']], 'tb_users')->result;
-                if ($ok){
-                    $this->database->commit();
-                    $res->data = [
-                        "token" => $toke,   
-                        "username" => $user["username"],
-                        "rule" => $user["rule"]
-                    ];
-                    $res->status = true;
-                }else{
-                    $res->message->title = "Ups, Lo sentimos";
-                    $res->message->content[] = "Ocurrio un error por favor intentalo nuevamente.";
-                }
+
+            // Validamos que el usuario no este bloqueado.
+            if ($user['disable'] == 1){
+                $res->message->title = "Si acceso";
+                $res->message->content[] = "Tu cuenta se encuentra deshabilitada.";
             }else{
-                
-                $res->message->content[] = "Contraseña incorrecta.";
+
+                if (password_verify($data->password, $user['password'])){
+                    
+                    // En caso de que la constraseña sea correcta.
+                    $toke = ApiFunctions::generateToken(50);
+                    $ok = $this->database->update(['token'=>$toke], ["id=?", $user['id']], 'tb_users')->result;
+                    if ($ok){
+                        $this->database->commit();
+                        $res->data = [
+                            "token" => $toke,   
+                            "username" => $user["username"],
+                            "rule" => $user["rule"]
+                        ];
+                        $res->status = true;
+                    }else{
+                        $res->message->title = "Ups, Lo sentimos";
+                        $res->message->content[] = "Ocurrio un error por favor intentalo nuevamente.";
+                    }
+                }else{
+                    
+                    $res->message->content[] = "Contraseña incorrecta.";
+                }
             }
         }else{
             $res->message->content[] = "El usuario no se encutra registrado.";
