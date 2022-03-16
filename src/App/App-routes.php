f5a742a7d6cf97a80867411f5e4048a2a6fce08e
HeilerNova <81369584+HeilerNova@users.noreply.github.com>
2022-03-16 16:22:27 -0500

    Se agrego la rutas para obtener EPS activas y modifcar los datos de una EPS

--- src/App/App-routes.php
+++ src/App/App-routes.php
@@ -12,7 +12,9 @@ Routes::post("auth", [AuthController::class, 'autenticate']);
 
 // Rutas del las EPS
 Routes::get("eps", [EpsController::class, 'getAll']);
+Routes::get("eps/active", [EpsController::class, 'getAllActive']);
 Routes::get("eps/{id:int}", [EpsController::class, 'get']);
+Routes::put("eps/{id:int}", [EpsController::class, 'update']);
 Routes::post("eps", [EpsController::class, 'insert']);
 Routes::patch("eps/{id:int}/disable", [EpsController::class, 'disable']);
 Routes::patch("eps/{id:int}/enable", [EpsController::class, 'enable']);
