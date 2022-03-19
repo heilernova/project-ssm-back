4bd94cfadb98734f4d90eb7029c83acd3ec8753d
HeilerNova <81369584+HeilerNova@users.noreply.github.com>
2022-03-19 10:27:32 -0500

    Se agrego las llaves foraneas a las tablas

--- install/db.sql
+++ install/db.sql
@@ -1,9 +1,14 @@
+SET FOREIGN_KEY_CHECKS = 0; -- Deshabilitamos las llaves foraneas
+
 -- Registro de usuarios.
 DROP TABLE IF EXISTS `tb_users`;
 CREATE TABLE `tb_users`
 (
     `id` INT PRIMARY KEY AUTO_INCREMENT,
     `date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    `dni` VARCHAR(15) NOT NULL UNIQUE,
+    `name` VARCHAR(20) NOT NULL,
+    `lastName` VARCHAR(20) NOT NULL,
     `username` VARCHAR(10) NOT NULL UNIQUE,
     `email` VARCHAR(150) NOT NULL UNIQUE,
     `password` VARCHAR(400) NOT NULL,
@@ -11,6 +16,10 @@ CREATE TABLE `tb_users`
     `rule` TINYINT NOT NULL,
     `disable` BIT
 );
+
+-- Insertamos el primero usuario
+INSERT INTO `tb_users`
+VALUES(1, NOW(),'1007244088', 'HEILER', 'NOVA', 'HEILERNOVA', 'HEILERNOVA@GMAIL.COM', '$2y$04$A8dGLVylvwo/0eLebRIam.jJ6xLqfrnMKay2m1xB7cmptEYAyGp9.', '', '1', 0);
 
 -- EPS
 DROP TABLE if EXISTS `tb_eps`;
@@ -25,10 +34,9 @@ CREATE TABLE `tb_eps`
 DROP TABLE if EXISTS `tb_persons`;
 CREATE TABLE `tb_persons`
 (
-    `dni` VARCHAR(15) PRIMARY KEY,
+    `dni` VARCHAR(15),
     `dniType` VARCHAR(5) NOT NULL,
     `date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-    `user` INT NOT NULL, -- usuario que lo registro
     `name` VARCHAR(20) NOT NULL,
     `lastName` VARCHAR(20) NOT NULL,
     `cellphone` VARCHAR(20) NOT NULL,
@@ -38,12 +46,16 @@ CREATE TABLE `tb_persons`
     `address` VARCHAR(50),
     `eps` INT NOT NULL,
     `sisben` BIT,
-    `regime` BIT
+    `regime` BIT,
+    INDEX `index_eps` (`eps`),
+    CONSTRAINT `fk_persons_eps` FOREIGN KEY(`eps`) REFERENCES `tb_eps`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+    PRIMARY KEY (`dni`)
 );
 
+
 -- Lista de servicios
-DROP TABLE if EXISTS `tb_services`;
-CREATE TABLE `tb_services`
+DROP TABLE if EXISTS `tb_requests_services`;
+CREATE TABLE `tb_requests_services`
 (
     `id` INT PRIMARY KEY AUTO_INCREMENT,
     `description` VARCHAR(50) NOT NULL
@@ -75,18 +87,33 @@ CREATE TABLE `tb_requests`
     `reinserted` BIT, -- Reincertado
     `palenRaizan` BIT,
     `roomGintano` BIT,
-    `nnaNunaccompaniedAdult` BIT
+    `nnaNunaccompaniedAdult` BIT,
+    INDEX `index_user` (`user`),
+    INDEX `index_service` (`service`),
+    INDEX `index_eps` (`eps`),
+    CONSTRAINT `fk_req_users` FOREIGN KEY (`user`) REFERENCES `tb_users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT `fk_req_service` FOREIGN KEY (`service`) REFERENCES `tb_requests_services`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT `fk_req_eps` FOREIGN KEY (`eps`) REFERENCES `tb_eps`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE
 );
+
 
 -- Historial de las observaciones acerca de las solicitudes.
 DROP TABLE if EXISTS `tb_request_observations`;
 CREATE TABLE `tb_request_observations`
 (
-    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `id` INT AUTO_INCREMENT,
     `date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    `user` INT NOT NULL,
     `request` INT NOT NULL,
-    `content` VARCHAR(200) NOT NULL
+    `content` VARCHAR(200) NOT NULL,
+    INDEX `index_request` (`request`),
+    INDEX `index_user` (`user`),
+    CONSTRAINT `fk_bservations_req`  FOREIGN KEY(`request`) REFERENCES `tb_requests`(`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT `fk_observations_users` FOREIGN KEY(`user`) REFERENCES `tb_users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+    PRIMARY KEY (`id`)
 );
+
+-- ALTER TABLE `tb_request_observations` ADD FOREIGN KEY(`request`) REFERENCES `persons`(`dni`);
 
 DROP VIEW if EXISTS `vi_requests`;
 CREATE VIEW `vi_requests` AS
@@ -97,3 +124,6 @@ CONCAT(`t2`.`name`, ' ', `t2`.`lastName`) AS `name`,
 `t2`.`email`
 FROM `tb_requests` `t1` 
 INNER JOIN `tb_persons` `t2` ON `t2`.`dni` = `t1`.`dni`;
+
+
+SET FOREIGN_KEY_CHECKS = 1; -- Deshabilitamos las llaves foraneas
