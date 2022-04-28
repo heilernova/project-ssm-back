731480c0f9af74d9ac605b7ae242bcbb703b1fbf
HeilerNova <81369584+HeilerNova@users.noreply.github.com>
2022-04-27 20:32:52 -0500

    Correción

--- src/Controllers/Surveys/ReportController.php
+++ src/Controllers/Surveys/ReportController.php
@@ -11,164 +11,204 @@ use PhpOffice\PhpSpreadsheet\Shared\Date as SharedDate;
 
 class ReportController extends BaseController
 {
-	function __construct(public ReportsDB $_report = new ReportsDB())
-	{
-		
-	}
+
+	/**
+	 * Method: <<< GET >>>
+	 * Retorna el total de las encuestas
+	 */
 	function getTotals(int $pollster, string $dateStart, string $dateEnd)
 	{
-		// return "Hola";
-		return $this->_report->getTotals($pollster, $dateStart, $dateEnd);  
+		$db = Api::getRouteConfig()->getDatabase();
+		$params = [];
+		$where = "";
+		if ($pollster > 0){
+			$where = "user=? AND ";
+			$params[] = $pollster;
+		}
+
+		$params[] = "$dateStart";
+		$params[] = "$dateEnd";
+		$where .= "`date` BETWEEN ? AND ?";
+
+		$result = ['total' => 0];
+		$result['eps'] = $db->execute("SELECT COUNT(*) FROM tb_surveys_eps WHERE $where", $params)->fetchArray()[0];
+		$result['ips'] = $db->execute("SELECT COUNT(*) FROM tb_surveys_ips WHERE $where", $params)->fetchArray()[0];
+		$result['hospitalization'] = $db->execute("SELECT COUNT(*) FROM tb_surveys_ips_hospitalization WHERE $where", $params)->fetchArray()[0];
+		$result['laboratory'] = $db->execute("SELECT COUNT(*) FROM tb_surveys_ips_laboratorys WHERE $where", $params)->fetchArray()[0];
+		$result['medicine'] = $db->execute("SELECT COUNT(*) FROM tb_surveys_ips_medicine WHERE $where", $params)->fetchArray()[0];
+		$result['odontology'] = $db->execute("SELECT COUNT(*) FROM tb_surveys_ips_odontology WHERE $where", $params)->fetchArray()[0];
+		$result['pharmacy'] = $db->execute("SELECT COUNT(*) FROM tb_surveys_ips_pharmacy WHERE $where", $params)->fetchArray()[0];
+		
+		foreach ($result as $value){
+			$result['total'] += $value;
+		}
+		return $result;
 	}
 
+	/**
+	 * Method: <<< GET >>>
+	 * Crea un archivo EXCEL con el reporte de las encuesta
+	 */
 	function get(int $pollster, string $dateStart, string $dateEnd)
 	{
-		// return "Hola munod";
-		$reports = $this->_report->getReports($pollster, $dateStart, $dateEnd);
 
-		try {
-			//code...
-			$temp =  Api::getConfig()->getDir() . "/../temp/Surveys.xlsx";
-			$reader = IOFactory::createReader("Xlsx");
-			$spreadsheet = $reader->load(Api::getConfig()->getDir() . "/../templates/Surveys.xlsx");
-		} catch (\Throwable $th) {
-			echo $th;
-			
+		$db = Api::getRouteConfig()->getDatabase();
+        $params = [];
+        $where = "";
+        if ($pollster > 0){
+            $where = "pollsterId=? AND ";
+            $params[] = $pollster;
+        }
+
+        $params[] = "$dateStart";
+        $params[] = "$dateEnd";
+        $where .= "`date` BETWEEN ? AND ?";
+
+        $result['eps'] = $db->execute("SELECT * FROM vi_surveys_eps WHERE $where", $params)->fetchAll();
+        $result['ips'] = $db->execute("SELECT * FROM vi_surveys_ips WHERE $where", $params)->fetchAll();
+        $result['hospitalization'] = $db->execute("SELECT * FROM vi_surveys_ips_hospitalization WHERE $where", $params)->fetchAll();
+        $result['laboratory'] = $db->execute("SELECT * FROM vi_surveys_ips_laboratorys WHERE $where", $params)->fetchAll();
+        $result['medicine'] = $db->execute("SELECT * FROM vi_surveys_ips_medicine WHERE $where", $params)->fetchAll();
+        $result['odontology'] = $db->execute("SELECT * FROM vi_surveys_ips_odontology WHERE $where", $params)->fetchAll();
+        $result['pharmacy'] = $db->execute("SELECT * FROM vi_surveys_ips_pharmacy WHERE $where", $params)->fetchAll();
+
+		$reports = $result;
+
+		$temp =  Api::getConfig()->getDir() . "/../temp/Surveys.xlsx";
+		$reader = IOFactory::createReader("Xlsx");
+		$spreadsheet = $reader->load(Api::getConfig()->getDir() . "/../templates/Surveys.xlsx");
+
+		// Cargamos las EPS
+		$sheet = $spreadsheet->getSheetByName("EPS");
+		$row = 3;
+		foreach ($reports['eps'] as $files){
+			unset($files['pollsterId']);
+			$colum = 1;
+			foreach ($files as $key=>$value){
+				if ($key == "date"){
+					$value = SharedDate::PHPToExcel($value);
+				}
+				$sheet->setCellValueByColumnAndRow($colum, $row, $value);
+				// $sheet->getColumnDimensionByColumn($colum)->setAutoSize(true);
+				$colum++; 
+			}
+
+			$row++;
 		}
-			//throw $th;
-		try {
-			// $sheet = $spreadsheet->getActiveSheet();
 
-			$sheet = $spreadsheet->getSheetByName("EPS");
-			$row = 3;
-			foreach ($reports['eps'] as $files){
-				unset($files['pollsterId']);
-				$colum = 1;
-				foreach ($files as $key=>$value){
-					if ($key == "date"){
-						$value = SharedDate::PHPToExcel($value);
-					}
-					$sheet->setCellValueByColumnAndRow($colum, $row, $value);
-					// $sheet->getColumnDimensionByColumn($colum)->setAutoSize(true);
-					$colum++; 
+		// Cargamos la IPS
+		$sheet = $spreadsheet->getSheetByName("IPS Primaria");
+		$row = 3;
+		foreach ($reports['ips'] as $files){
+			unset($files['pollsterId']);
+			$colum = 1;
+			foreach ($files as $key=>$value){
+				if ($key == "date"){
+					$value = SharedDate::PHPToExcel($value);
 				}
-
-				$row++;
+				$sheet->setCellValueByColumnAndRow($colum, $row, $value);
+				// $sheet->getColumnDimensionByColumn($colum)->setAutoSize(true);
+				$colum++; 
 			}
 
-			// Ips primaria
-			$sheet = $spreadsheet->getSheetByName("IPS Primaria");
-			$row = 3;
-			foreach ($reports['ips'] as $files){
-				unset($files['pollsterId']);
-				$colum = 1;
-				foreach ($files as $key=>$value){
-					if ($key == "date"){
-						$value = SharedDate::PHPToExcel($value);
-					}
-					$sheet->setCellValueByColumnAndRow($colum, $row, $value);
-					// $sheet->getColumnDimensionByColumn($colum)->setAutoSize(true);
-					$colum++; 
-				}
-
-				$row++;
-			}
-
-			$sheet = $spreadsheet->getSheetByName("Hospitalizaciones");
-			$row = 3;
-			foreach ($reports['hospitalization'] as $files){
-				$colum = 1;
-				unset($files['pollsterId']);
-				foreach ($files as $key=>$value){
-					if ($key == "date"){
-						$value = SharedDate::PHPToExcel($value);
-					}
-					$sheet->setCellValueByColumnAndRow($colum, $row, $value);
-					// $sheet->getColumnDimensionByColumn($colum)->setAutoSize(true);
-					$colum++; 
-				}
-
-				$row++;
-			}
-			
-			$sheet = $spreadsheet->getSheetByName("Laboratorio");
-			$row = 3;
-			foreach ($reports['laboratory'] as $files){
-				$colum = 1;
-				unset($files['pollsterId']);
-				foreach ($files as $key => $value){
-					
-					if ($key == "date"){
-						$value = SharedDate::PHPToExcel($value);
-					}
-					$sheet->setCellValueByColumnAndRow($colum, $row, $value);
-					// $sheet->getColumnDimensionByColumn($colum)->setAutoSize(true);
-					$colum++; 
-				}
-
-				$row++;
-			}
-			
-			$sheet = $spreadsheet->getSheetByName("Medicina");
-			$row = 3;
-			foreach ($reports['medicine'] as $files){
-				$colum = 1;
-				unset($files['pollsterId']);
-				foreach ($files as $key=>$value){
-					if ($key == "date"){
-						$value = SharedDate::PHPToExcel($value);
-					}
-					$sheet->setCellValueByColumnAndRow($colum, $row, $value);
-					// $sheet->getColumnDimensionByColumn($colum)->setAutoSize(true);
-					$colum++; 
-				}
-
-				$row++;
-			}
-			
-			$sheet = $spreadsheet->getSheetByName("Odontología");
-			$row = 3;
-			foreach ($reports['odontology'] as $files){
-				$colum = 1;
-				unset($files['pollsterId']);
-				foreach ($files as $key=>$value){
-
-					if ($key == "date"){
-						$value = SharedDate::PHPToExcel($value);
-					}
-					$sheet->setCellValueByColumnAndRow($colum, $row, $value);
-					// $sheet->getColumnDimensionByColumn($colum)->setAutoSize(true);
-					$colum++; 
-				}
-
-				$row++;
-			}
-
-			$sheet = $spreadsheet->getSheetByName("Farmacia");
-			$row = 3;
-			foreach ($reports['pharmacy'] as $files){
-				$colum = 1;
-				unset($files['pollsterId']);
-				foreach ($files as $key=>$value){
-					if ($key == "date"){
-						$value = SharedDate::PHPToExcel($value);
-					}
-					$sheet->setCellValueByColumnAndRow($colum, $row, $value);
-					// $sheet->getColumnDimensionByColumn($colum)->setAutoSize(true);
-					$colum++; 
-				}
-				$row++;
-			}
-			
-
-
-			$writer = IOFactory::createWriter($spreadsheet, 'Xlsx');
-			$writer->save($temp);
-
-			Response::setResponsetype('blob');
-			return $temp;
-		} catch (\Exception $e) {
-			return 'Ocurrió un error al intentar abrir el archivo ' . $e;
+			$row++;
 		}
+
+		// Cargamos las Hostitalizaciones
+		$sheet = $spreadsheet->getSheetByName("Hospitalizaciones");
+		$row = 3;
+		foreach ($reports['hospitalization'] as $files){
+			$colum = 1;
+			unset($files['pollsterId']);
+			foreach ($files as $key=>$value){
+				if ($key == "date"){
+					$value = SharedDate::PHPToExcel($value);
+				}
+				$sheet->setCellValueByColumnAndRow($colum, $row, $value);
+				// $sheet->getColumnDimensionByColumn($colum)->setAutoSize(true);
+				$colum++; 
+			}
+
+			$row++;
+		}
+		
+		// Cargamos las laboratorio
+		$sheet = $spreadsheet->getSheetByName("Laboratorio");
+		$row = 3;
+		foreach ($reports['laboratory'] as $files){
+			$colum = 1;
+			unset($files['pollsterId']);
+			foreach ($files as $key => $value){
+				
+				if ($key == "date"){
+					$value = SharedDate::PHPToExcel($value);
+				}
+				$sheet->setCellValueByColumnAndRow($colum, $row, $value);
+				// $sheet->getColumnDimensionByColumn($colum)->setAutoSize(true);
+				$colum++; 
+			}
+
+			$row++;
+		}
+		
+		// Cargamos medicia general
+		$sheet = $spreadsheet->getSheetByName("Medicina");
+		$row = 3;
+		foreach ($reports['medicine'] as $files){
+			$colum = 1;
+			unset($files['pollsterId']);
+			foreach ($files as $key=>$value){
+				if ($key == "date"){
+					$value = SharedDate::PHPToExcel($value);
+				}
+				$sheet->setCellValueByColumnAndRow($colum, $row, $value);
+				// $sheet->getColumnDimensionByColumn($colum)->setAutoSize(true);
+				$colum++; 
+			}
+
+			$row++;
+		}
+		
+		// Cargamos odontología
+		$sheet = $spreadsheet->getSheetByName("Odontología");
+		$row = 3;
+		foreach ($reports['odontology'] as $files){
+			$colum = 1;
+			unset($files['pollsterId']);
+			foreach ($files as $key=>$value){
+
+				if ($key == "date"){
+					$value = SharedDate::PHPToExcel($value);
+				}
+				$sheet->setCellValueByColumnAndRow($colum, $row, $value);
+				// $sheet->getColumnDimensionByColumn($colum)->setAutoSize(true);
+				$colum++; 
+			}
+
+			$row++;
+		}
+
+		// Cargamos la farmacia
+		$sheet = $spreadsheet->getSheetByName("Farmacia");
+		$row = 3;
+		foreach ($reports['pharmacy'] as $files){
+			$colum = 1;
+			unset($files['pollsterId']);
+			foreach ($files as $key=>$value){
+				if ($key == "date"){
+					$value = SharedDate::PHPToExcel($value);
+				}
+				$sheet->setCellValueByColumnAndRow($colum, $row, $value);
+				// $sheet->getColumnDimensionByColumn($colum)->setAutoSize(true);
+				$colum++; 
+			}
+			$row++;
+		}
+		
+		$writer = IOFactory::createWriter($spreadsheet, 'Xlsx');
+		$writer->save($temp);
+
+		Response::setResponsetype('blob');
+		return $temp;
 	}
 }
